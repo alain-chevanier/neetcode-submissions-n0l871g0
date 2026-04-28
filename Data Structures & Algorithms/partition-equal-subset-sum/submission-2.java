e9f6a59e4e4d642e0dabf9b1b15714eae67ae7a7
Alain Chevanier <alain.chevanier@gmail.com>
2026-04-28 00:50:11 -0600

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.java
@@ -0,0 +1,44 @@
+class Solution {
+    int[] nums;
+    Boolean[][] memo;
+
+    public boolean canPartition(int[] nums) {
+        this.nums = nums;
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+
+        if (sum % 2 == 1) {
+            return false;
+        }
+
+        Arrays.sort(nums);
+
+        this.memo = new Boolean[nums.length][sum/2+1];
+        return aux(0, sum/2);
+    }
+
+    boolean aux(int idx, int targetSum) {
+        if (targetSum == 0) {
+            return true;
+        }
+        if (idx == this.nums.length || targetSum <  0) {
+            return false;
+        }
+
+        if (this.memo[idx][targetSum] != null) {
+            return this.memo[idx][targetSum];
+        }
+
+        if (this.nums[idx] > targetSum) {
+            return false;
+        }
+
+        this.memo[idx][targetSum] = 
+            aux(idx + 1, targetSum) // skip it
+            || aux(idx + 1, targetSum - this.nums[idx]); // take it
+
+        return this.memo[idx][targetSum];
+    }
+}
