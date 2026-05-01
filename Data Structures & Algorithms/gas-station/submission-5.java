883610ea1f48499352a22bdb8ea160e743d1b3be
Alain Chevanier <alain.chevanier@gmail.com>
2026-05-01 17:12:27 -0600

    Add: gas-station - submission-5

---
+++ Data Structures & Algorithms/gas-station/submission-5.java
@@ -0,0 +1,60 @@
+class Solution {
+    int N;
+    int[] gas;
+    int[] cost;
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        this.N = gas.length;
+        this.gas = gas;
+        this.cost = cost;
+
+        int maxIdx = 0,
+            maxDiff = gas[0] - cost[0],
+            minIdx = 0,
+            minDiff = gas[0] - cost[0];
+        for (int i = 1; i < N; i++) {
+            int diff = gas[i] - cost[i];
+            if (diff > maxDiff) {
+                maxDiff = diff;
+                maxIdx = i;
+            }
+
+            if (diff <= minDiff) {
+                minDiff = diff;
+                minIdx = i;
+            }
+        }
+
+        if (maxDiff < 0) {
+            return -1;
+        }
+
+        if (aux(maxIdx)) {
+            return maxIdx;
+        }
+
+        if (aux(minIdx + 1)) {
+            return mod(minIdx + 1, N);
+        }
+
+        return -1;
+    }
+
+    boolean aux(int i) {
+        i = mod (i, N);
+        int curGas = 0;
+        int size = N;
+        while (size-- > 0) {
+            curGas += gas[i] - cost[i];
+            if (curGas < 0) {
+                return false;
+            }
+            i = mod(i+1, N);
+        }
+        return true;
+    }
+
+    int mod(int i, int n) {
+        return i >= 0 ? (i%n) : (n+(i%n))%n;
+    }
+}
